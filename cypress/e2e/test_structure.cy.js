a0e2e99e1c937e2cf0b00ac2c6897cc58baae75c
sammythedeveloper <justsammy484@gmail.com>
2025-05-27 16:07:20 -0400

    testing website

---
+++ cypress/e2e/test_structure.cy.js
@@ -0,0 +1,22 @@
+describe("Test suite one", () => {
+    it("Test case one", () => {
+        //what we want to test
+    })
+    it("Test case two", () => {
+        //
+    })
+    it("Test case three", () => {
+        //
+    })
+})
+describe("Test suite Two", () => {
+    it("Test case one", () => {
+        //what we want to test
+    })
+    it("Test case two", () => {
+        //
+    })
+    it("Test case three", () => {
+        //
+    })
+})
